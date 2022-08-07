f4631fbc50c3ec4ed578ec7029f061c5b7c8d6b4
deffelovers <deffelovers.code@gmail.com>
2022-08-07 16:30:22 +0700

    push to github

---
+++ plugins/sewa.js
@@ -0,0 +1,59 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let sewa = `*────── 「 LIST SEWA 」 ──────*
+
+Hai 👋
+𝐈𝐧𝐢 𝐥𝐢𝐬𝐭 𝐬𝐞𝐰𝐚 𝐤𝐚𝐦𝐢 𝐬𝐢𝐥𝐚𝐡𝐤𝐚𝐧 𝐝𝐢𝐩𝐢𝐥𝐢𝐡
+
+┏━━━•❅•°•❈〔 𝐋𝐢𝐬𝐭 𝐒𝐞𝐰𝐚 〕
+┣★ミ Premium : 10k/Bulan
+┣★ミ Premium : 15k/2bulan
+┣★ミ Undang bot ke grup : 5k/bulan
+┣➲ Scan Barcod Qrisnya diatas🤗
+┗━━━•❅•°•❈
+
+┏━━━•❅•°•❈𝐊𝐞𝐮𝐧𝐭𝐮𝐧𝐠𝐚𝐧 𝐌𝐞𝐦𝐛𝐞𝐫 𝐏𝐫𝐞𝐦𝐢𝐮𝐦
+┣➲ Unlock Fitur Premium
+┣➲ limit didapat tiap hari dengan claim
+┣➲ bisa invite bot ke grup
+┗━━━•❅•°•❈
+Contact person Owner:
+wa.me/6285798048936 (Owner)`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/d6b8766969db8c9abef0d.jpg' }}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: sewa,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+
+           },
+               {
+             callButton: {
+               displayText: 'Gopay',
+               phoneNumber: '085798048936'
+             }
+           },           
+               {
+             quickReplyButton: {
+               displayText: '🧒 Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.help = ['sewa']
+handler.tags = ['donasi']
+handler.command = /^sewa$/i
+
+module.exports = handler
